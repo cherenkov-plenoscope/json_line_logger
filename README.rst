0368e2b24c965c8792515e0643dd6fdb0ecbb08e
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-06-08 16:26:10 +0200

    get started

---
+++ README.rst
@@ -0,0 +1,10 @@
+Write logs into JSON-lines
+==========================
+|BlackStyle|
+
+Use python's awesome logging-library and configure it to write JSON-lines.
+In JSONL, each line in the log is a valid json-object.
+This makes the log easy to read with a machine.
+
+.. |BlackStyle| image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :target: https://github.com/psf/black
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="json_line_logger_sebastian-achim-mueller",
+    version="0.0.1",
+    description="Configs for python's logging library to have a JSONL-log",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/cherenkov-plenoscope/json_line_logger",
+    project_urls={
+        "Bug Tracker": "https://github.com/cherenkov-plenoscope/json_line_logger/issues",
+    },
+    author="Sebastian Achim Mueller",
+    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
+    packages=["json_line_logger",],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+)
